aadca37b5761e2d9f6b8a992a47c702ff8128101
ikinoti <isaiah.gitonga@student.moringaschool.com>
2021-09-25 20:18:50 +0300

    logout function

--- app/auth/views.py
+++ app/auth/views.py
@@ -31,3 +31,9 @@ def login():
     
     title = "Login to 60 Seconds"
     return render_template("auth/login.html",login_form = login_form,title = title)
+
+@auth.route("/logout")
+@login_required
+def logout():
+    logout_user()
+    return redirect(url_for("main.index"))
