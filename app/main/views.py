89d6f84e76187b1ef4101df499ffe72557767c9c
ikinoti <isaiah.gitonga@student.moringaschool.com>
2021-09-25 17:59:49 +0300

    add blog view function"

--- app/main/views.py
+++ app/main/views.py
@@ -25,3 +25,30 @@ def index():
     return render_template("index.html",
                             posts = posts,
                             quote = quote)
+
+@main.route("/post/<int:id>", methods = ["POST", "GET"])
+def post(id):
+    post = Post.query.filter_by(id = id).first()
+    comments = Comment.query.filter_by(post_id = id).all()
+    comment_form = CommentForm()
+    comment_count = len(comments)
+
+    if comment_form.validate_on_submit():
+        comment = comment_form.comment.data
+        comment_form.comment.data = ""
+        comment_alias = comment_form.alias.data
+        comment_form.alias.data = ""
+        if current_user.is_authenticated:
+            comment_alias = current_user.username
+        new_comment = Comment(comment = comment, 
+                            comment_at = datetime.now(),
+                            comment_by = comment_alias,
+                            post_id = id)
+        new_comment.save_comment()
+        return redirect(url_for("main.post", id = post.id))
+
+    return render_template("post.html",
+                            post = post,
+                            comments = comments,
+                            comment_form = comment_form,
+                            comment_count = comment_count)
