d0914b4af7598e23219056fbf1c2bbfcc5f858bc
ikinoti <isaiah.gitonga@student.moringaschool.com>
2021-09-25 18:02:01 +0300

    add comment view function"

--- app/main/views.py
+++ app/main/views.py
@@ -22,9 +22,7 @@ def index():
         db.session.commit()
         welcome_message("Thank you for subscribing to the MMM blog", 
                         "email/welcome", new_sub.email)
-    return render_template("index.html",
-                            posts = posts,
-                            quote = quote)
+    return render_template("index.html",posts = posts,quote = quote)
 
 @main.route("/post/<int:id>", methods = ["POST", "GET"])
 def post(id):
@@ -40,15 +38,45 @@ def post(id):
         comment_form.alias.data = ""
         if current_user.is_authenticated:
             comment_alias = current_user.username
-        new_comment = Comment(comment = comment, 
-                            comment_at = datetime.now(),
-                            comment_by = comment_alias,
-                            post_id = id)
+        new_comment = Comment(comment = comment,comment_at = datetime.now(),comment_by = comment_alias,post_id = id)
         new_comment.save_comment()
         return redirect(url_for("main.post", id = post.id))
 
-    return render_template("post.html",
-                            post = post,
-                            comments = comments,
-                            comment_form = comment_form,
-                            comment_count = comment_count)
+    return render_template("post.html",post = post,comments = comments,comment_form = comment_form,comment_count = comment_count)
+
+@main.route("/post/<int:id>/<int:comment_id>/delete")
+def delete_comment(id, comment_id):
+    post = Post.query.filter_by(id = id).first()
+    comment = Comment.query.filter_by(id = comment_id).first()
+    db.session.delete(comment)
+    db.session.commit()
+    return redirect(url_for("main.post", id = post.id))
+
+@main.route("/post/<int:id>/<int:comment_id>/favourite")
+def fav_comment(id, comment_id):
+    post = Post.query.filter_by(id = id).first()
+    comment = Comment.query.filter_by(id = comment_id).first()
+    comment.like_count = 1
+    db.session.add(comment)
+    db.session.commit()
+    return redirect(url_for('main.post', id = post.id))
+
+@main.route("/post/<int:id>/update", methods = ["POST", "GET"])
+@login_required
+def edit_post(id):
+    post = Post.query.filter_by(id = id).first()
+    edit_form = UpdatePostForm()
+
+    if edit_form.validate_on_submit():
+        post.post_title = edit_form.title.data
+        edit_form.title.data = ""
+        post.post_content = edit_form.post.data
+        edit_form.post.data = ""
+
+        db.session.add(post)
+        db.session.commit()
+        return redirect(url_for("main.post", id = post.id))
+
+    return render_template("edit_post.html", post = post,edit_form = edit_form)
+
+
