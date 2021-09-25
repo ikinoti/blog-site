f839c5cc5513a44aa649304e2749fffb0c1469b1
ikinoti <isaiah.gitonga@student.moringaschool.com>
2021-09-25 18:07:49 +0300

    form for posting a blog

---
+++ app/main/forms.py
@@ -0,0 +1,11 @@
+
+  
+from flask_wtf import FlaskForm
+from wtforms import (StringField, TextAreaField,
+                    SubmitField, SelectField)
+from wtforms.validators import Required
+
+class PostForm(FlaskForm):
+    title = StringField("Blog title:", validators=[Required()])
+    post = TextAreaField("Type Away:", validators=[Required()])
+    submit = SubmitField("Post")
