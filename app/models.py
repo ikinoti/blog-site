0ae04f7db9e83f97ccc57ee46a5ee9f05d4ec77e
ikinoti <isaiah.gitonga@student.moringaschool.com>
2021-09-25 16:03:29 +0300

    add a blog class model

--- app/models.py
+++ app/models.py
@@ -62,3 +62,36 @@ class User(UserMixin, db.Model):
     # string representaion to print out a row of a column, important in debugging
     def __repr__(self):
         return f"User {self.username}"
+
+class Post(db.Model):
+    __tablename__ = "posts"
+
+    id = db.Column(db.Integer, primary_key = True)
+    post_title = db.Column(db.String)
+    post_content = db.Column(db.Text)
+    posted_at = db.Column(db.DateTime)
+    upvotes = db.Column(db.Integer, default = 0)
+    downvotes = db.Column(db.Integer, default = 0)
+    post_by = db.Column(db.String)
+    user_id = db.Column(db.Integer, db.ForeignKey("users.id"))
+    comments = db.relationship("Comment", 
+                                foreign_keys = "Comment.post_id", 
+                                backref = "post", 
+                                lazy = "dynamic")
+
+    def save_post(self):
+        db.session.add(self)
+        db.session.commit()
+
+    def delete_post(self):
+        db.session.delete(self)
+        db.session.commit()
+
+    @classmethod
+    def get_user_posts(cls,id):
+        posts = Post.query.filter_by(user_id = id).order_by(Post.posted_at.desc()).all()
+        return posts
+
+    @classmethod
+    def get_all_posts(cls):
+        return Post.query.order_by(Post.posted_at).all()
